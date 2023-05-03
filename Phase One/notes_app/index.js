d2ecc2928d97ca6dd6f439b3a2380393771a0dfe
djohn06 <danielajohn1406@gmail.com>
2023-05-03 10:40:38 +0100

    Ph1.4 | Added functions & items into console < Challenge complete

--- Phase One/notes_app/index.js
+++ Phase One/notes_app/index.js
@@ -1,1 +1,8 @@
+const NotesModel = require('./notesModel')
+
 console.log("The notes app is running");
+
+model = new NotesModel
+  model.addNotes('Buy milk');
+  model.addNotes('Go to the gym');
+console.log(model.getNotes());
