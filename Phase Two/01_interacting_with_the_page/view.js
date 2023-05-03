20f577b06f27365b5f25ecd16b60e6f9ccfbcfda
djohn06 <danielajohn1406@gmail.com>
2023-05-03 15:30:33 +0100

    Ph2.1 | Created two new functions: addParagraph & clearParagraphs

--- Phase Two/01_interacting_with_the_page/view.js
+++ Phase Two/01_interacting_with_the_page/view.js
@@ -4,6 +4,19 @@ class View {
 
     console.log(this.mainContainerEl);
   }
+
+  addParagraph() {
+    const newParagraph = document.createElement("p");
+    newParagraph.textContent = "This paragraph has been dynamically added by JavaScript!";
+    this.mainContainerEl.append(newParagraph);
+  }
+
+  clearParagraphs() {
+    const removeParagraphs = document.querySelectorAll('p');
+    removeParagraphs.forEach(paragraph => {
+      paragraph.remove();
+    });
+  }
 }
 
 module.exports = View;
