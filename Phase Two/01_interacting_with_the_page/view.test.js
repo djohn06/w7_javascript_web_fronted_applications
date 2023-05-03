6e71225aa5bf4d97f30e0b50b59bea7c9a0ecfa7
djohn06 <danielajohn1406@gmail.com>
2023-05-03 15:31:09 +0100

    Ph2.1 | Passed tests calling both new functions

--- Phase Two/01_interacting_with_the_page/view.test.js
+++ Phase Two/01_interacting_with_the_page/view.test.js
@@ -2,15 +2,34 @@
  * @jest-environment jsdom
  */
 
-const fs = require('fs');
+const fs = require('fs'); // 'fs' is a built in node module which is required to read non-JavaScript files.
 const View = require('./view');
 
 describe('Page view', () => {
+
+  beforeEach(() => {
+    document.body.innerHTML = fs.readFileSync("./index.html");
+  })
+
   it('displays 2 paragraphs', () => {
-    document.body.innerHTML = fs.readFileSync('./index.html');
-
-    const view = new View();
-
     expect(document.querySelectorAll('p').length).toBe(2);
   });
+
+  it('displays a third paragraph', () => {
+    // Arrange
+    const view = new View();
+    // Act
+    view.addParagraph();
+    // Assert
+    expect(document.querySelectorAll('p').length).toBe(3);
+  })
+
+  it('clears all paragraphs', () => {
+    // Arrange
+    const view = new View();
+    // Act
+    view.clearParagraphs();
+    // Assert
+    expect(document.querySelectorAll('p').length).toBe(0);
+  })
 });
