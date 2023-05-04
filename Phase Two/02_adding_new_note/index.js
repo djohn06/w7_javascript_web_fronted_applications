6ae93fff9fb2da1d174b2e80172b0a2ff4bf885e
djohn06 <danielajohn1406@gmail.com>
2023-05-04 15:44:29 +0100

    Ph03 | Renamed directory

--- Phase Two/02_adding_new_note/index.js
+++
@@ -1,8 +0,0 @@
-const NotesModel = require('./notesModel')
-
-console.log("The notes app is running");
-
-model = new NotesModel
-  model.addNotes('Buy milk');
-  model.addNotes('Go to the gym');
-console.log(model.getNotes());
