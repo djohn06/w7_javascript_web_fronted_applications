3df7c9eeb2852fe830afd50363dda67994712c39
djohn06 <danielajohn1406@gmail.com>
2023-05-04 17:21:56 +0100

    Ph2.4 | Updated view files to include 'hide message'

--- Phase Two/04_user_interaction/messageView.js
+++ Phase Two/04_user_interaction/messageView.js
@@ -1,14 +1,26 @@
 class MessageView {
   constructor() {
     this.buttonEl = document.querySelector('#show-message-button');
+    this.hideButtonEl = document.querySelector('#hide-message-button');
 
     this.buttonEl.addEventListener('click', () => {
        this.displayMessage();
+    });
+    this.hideButtonEl.addEventListener('click', () => {
+      this.hideMessage();
     });
   }
 
   displayMessage() {
     console.log('Thanks for clicking me!');
+    const messageEl = document.createElement('div');
+    messageEl.id = 'message';
+    messageEl.textContent = "This message displayed by JavaScript.";
+    document.querySelector('#main-container').append(messageEl);
+  }
+
+  hideMessage() {
+    document.querySelector('#message').remove();
   }
 }
 
--- Phase Two/04_user_interaction/messageView.test.js
+++ Phase Two/04_user_interaction/messageView.test.js
@@ -6,7 +6,7 @@ const fs = require('fs');
 const MessageView = require('./messageView');
 
 describe('MessageView', () => {
-  it('clicks the button', () => {
+  it('clicks the button, show message', () => {
     document.body.innerHTML = fs.readFileSync('./index.html');
 
     const view = new MessageView();
@@ -15,5 +15,18 @@ describe('MessageView', () => {
     buttonEl.click();
 
     expect(document.querySelector('#message')).not.toBeNull();
+  });
+
+  it('clicks the button, hide message', () => {
+    document.body.innerHTML = fs.readFileSync('./index.html');
+
+    const view = new MessageView();
+
+    const buttonEl = document.querySelector('#show-message-button');
+    buttonEl.click();
+    const hideMessage = document.querySelector('#hide-message-button');
+    hideMessage.click();
+
+    expect(document.querySelector('#message')).toBeNull();
   });
 });
